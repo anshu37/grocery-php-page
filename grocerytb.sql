f111d00ecb27d28a88a291b39c3d173dea925f16
anshu37 <59794423+anshu37@users.noreply.github.com>
2020-06-02 08:25:25 +0530

    added php page

---
+++ grocerytb.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 10, 2020 at 02:10 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `grocerydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grocerytb`
+--
+
+CREATE TABLE `grocerytb` (
+  `Id` int(10) NOT NULL,
+  `Item_name` varchar(30) NOT NULL,
+  `Item_Quantity` int(100) NOT NULL,
+  `Item_status` varchar(20) NOT NULL,
+  `Date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `grocerytb`
+--
+
+INSERT INTO `grocerytb` (`Id`, `Item_name`, `Item_Quantity`, `Item_status`, `Date`) VALUES
+(12, 'tomato', 5, '1', '2020-04-06'),
+(14, 'spniach', 2, '1', '2020-04-07'),
+(15, 'cauliflower', 6, '2', '2020-04-09'),
+(16, 'cabbage', 7, '1', '2020-04-08');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `grocerytb`
+--
+ALTER TABLE `grocerytb`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `grocerytb`
+--
+ALTER TABLE `grocerytb`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
